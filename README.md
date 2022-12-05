cba0aa06e946d2a7eddc70ce30077e5ad54f9db0
aniqurahman <aniqurrahman@outlook.com>
2022-12-05 22:21:38 +0300

    News dynamic fetching and topic, link, heading and body storing in CSV

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# newsscapper
-Dawn News Web Scapper
+# Dawn News Scapper
+
+This scapper can be used to easily fetch the dawn news data for the selected days, and the results will be store in an CSV file, along with date, topic, link, title and body. 
+
+
+
+## Motivation
+
+MS research work for Time-series based Fake news analysis
+
+
+## Running instructions
+
+The number of day's variable will get the previous day's news.
+```
+NUMBER_OF_DAYS = 200
+```
+
+Custom Topics can be added by changing the list of baseURL's
+```
+baseURLList=["https://www.dawn.com/newspaper/national/","https://www.dawn.com/newspaper/business/", "https://www.dawn.com/newspaper/sport/"]
+```
+
+That's it, after execution the dataset will be created, tehre is a delay added in to handle the connection limit from Dawn news.
+
+![Dataset Example](./Dataset.png)
--- newScapper.py
+++ newScapper.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 import datetime
 import pandas as pd
 import csv
+import re
 from time import sleep
 
-NUMBER_OF_DAYS = 30
+NUMBER_OF_DAYS = 200
 countOfNews = 0
 
 def create_dataset_file():
@@ -17,13 +18,15 @@ def create_dataset_file():
         writer = csv.writer(csvfile)
 
         # Write the data rows
-        writer.writerow(["DateOfNews", "LinkOfNews", "HeadingOfNews", "BodyOfNews"])
+        writer.writerow(["DateOfNews", "Topic", "LinkOfNews", "HeadingOfNews", "BodyOfNews"])
     csvfile.close()
 
-def save_raw_news_to_dataset(date, link, heading, body):
+def save_raw_news_to_dataset(date, rawTopic, link, heading, body):
+    tokenTopic = re.split('/',rawTopic)
+    topic = tokenTopic[4]
     with open('RAW_NEWS.csv', 'a', newline='\n') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow([date, link, heading, body])
+        writer.writerow([date, topic, link, heading, body])
     csvfile.close()
 
 
@@ -34,61 +37,65 @@ for i in range(1,NUMBER_OF_DAYS):
     #calculate the date 30 days before
     url = ""
     date = now - datetime.timedelta(days= i)
-    # https://www.dawn.com/newspaper/national/2022-12-01
-    # https://www.dawn.com/newspaper/business/2022-12-01
-    # https://www.dawn.com/newspaper/sport/2022-12-02
     dateOfNews = "{0:02d}-{1:02d}-{2:02d}".format(date.year, date.month,date.day)
-    #construct the url for scraping
-    # url = "https://www.dawn.com/archive/" + dateOfNews
-    baseURL = "https://www.dawn.com/archive/"
 
-    # baseURL = "https://www.dawn.com/newspaper/national/"
-    url = str(baseURL + dateOfNews)
-    print("Archive News URL: ", url)
-    sleep(10)
-    #request the web page
-    resp = requests.get(url)
-    print("Archive News Response: ", resp)
-    if resp.status_code > 204:
-       print("Archive News URL Not accessible!")
-    #parse the page using beautifulsoup
-    soup = BeautifulSoup(resp.text, 'html.parser')
+    baseURLList =  [
+                    "https://www.dawn.com/newspaper/national/",
+                    "https://www.dawn.com/newspaper/business/", 
+                    "https://www.dawn.com/newspaper/sport/"
+                    ]
 
-    #find all the links present in the page
-    links = soup.find_all('a')
+    for _indexBaseURL in range(0,len(baseURLList)):
+        url = str(baseURLList[_indexBaseURL] + dateOfNews)
+        print("Archive News URL: ", url)
+        sleep(10)
+        #request the web page
+        resp = requests.get(url)
+        print("Archive News Response: ", resp)
+        if resp.status_code > 204:
+            print("Archive News URL Not accessible!")
+            #parse the page using beautifulsoup
+        else:
+            soup = BeautifulSoup(resp.text, 'html.parser')
+            #find all the links present in the page
+            links = soup.find_all('a')
 
-    #iterate through the links and find the ones which are related to politics
-    for link in links:
-        
-        try:
-            if len(link['href']) > 70 and "dawn.com" in link['href']:
-                try:      
-                    news_url = link['href']
-                    #make request for the news page
-                    resp = requests.get(news_url)
-                    #parse the page using beautiful soup
-                    soup = BeautifulSoup(resp.text, 'html.parser')
-                    #find the div which contains the news body
-                    news_body = soup.find('div', {'class': 'story__content'})
-                    #print the news body
-                    dateNews = str(date.strftime("%d/%m/%Y"))
-                    newslink = news_url
-                    newsheading = str(link.text)
-                    newsbody = str(news_body.text)
-                    # newsTopic = str(news_body.title)
+            #iterate through the links and find the ones which are related to politics
+            for link in links:
+                try:
+                    # if len(link['href']) > 70 and "dawn.com" in link['href'] and "#comments" not in link['href'] and "read full story" not in link['href']:
+                    if len(link['href']) > 70 and "dawn.com" in link['href'] and "read full story" in link: 
+                        try:      
+                            news_url = link['href']
+                            #make request for the news page
+                            resp = requests.get(news_url)
+                            #parse the page using beautiful soup
+                            soup = BeautifulSoup(resp.text, 'html.parser')
+                            
+                            rawNewsheading = soup.find('title') 
+                            newsheading = str(rawNewsheading.text)
 
-                    print("-----")
-                    print("Date: ", dateNews)
-                    print("Link: ", newslink)
-                    # print("Heading: " ,newsheading)
-                    # print("Body: ", newsbody)
+                            #find the div which contains the news body
+                            news_body = soup.find('div', {'class': 'story__content'})
+                            #print the news body
+                            dateNews = str(date.strftime("%d/%m/%Y"))
+                            newslink = news_url
+                            # newsheading = str(link.text)
+                            newsbody = str(news_body.text)
+                            # newsTopic = str(news_body.title)
 
-                    save_raw_news_to_dataset(dateNews, link.attrs['href'], newsheading, newsbody)
-                    
-                    countOfNews += 1
-                    print("News Count: ", countOfNews)
-                    
+                            print("-----")
+                            print("Date: ", dateNews)
+                            print("Link: ", newslink)
+                            # print("Heading: " ,newsheading)
+                            # print("Body: ", newsbody)
+
+                            save_raw_news_to_dataset(dateNews, baseURLList[_indexBaseURL], link.attrs['href'], newsheading, newsbody)
+                            
+                            countOfNews += 1
+                            print("News Count: ", countOfNews)
+                            
+                        except:
+                            pass
                 except:
                     pass
-        except:
-            pass
